fbd0c6301ac2b4ba68f738f4185de1fa270dfec3
tanuxa-lis <tanuxa-lis@mail.ru>
2024-11-07 10:58:57 +0700

    группировка и  агрегатные функции

--- poperators_in_between_.sql
+++ poperators_in_between_.sql
@@ -110,3 +110,95 @@ FROM
     `shop`.`customers`
 WHERE
     cname LIKE '%/%%' escape "/";
+
+  /*агрегатные функции
+    -COUNT()- счётчик
+    -SUM()
+    -AVG()- среднее значение
+    -MAX()
+    -MIN()*/
+   
+/*подсчёт суммы чисел по определённому столбцу*/
+
+SELECT 
+    SUM(amount)
+FROM
+    sales;
+ 
+ /*подсчёт количества строк с условием*/
+SELECT 
+    COUNT(*)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-01'
+        AND sdate <= '2024-09-30';
+	        
+	 /*вычисление среднего значения с условием*/        
+SELECT 
+    AVG(amount)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-01'
+        AND sdate <= '2024-09-30';
+	 
+	 /*при использовании distinct внутри функции count() подсчитываются только уникальные значения в столбце*/
+ SELECT
+    COUNT(distinct city)
+FROM
+    customers;
+ 
+ /*функции мах и мин работают со строковыми значениями, ориентируясь на строковый порядок*/
+SELECT
+    max(cname)
+FROM
+    customers;
+
+
+/*группировка максимальных значений продаж по каждому продавцу */
+SELECT 
+    snam, MAX(amount)
+FROM
+    sales
+GROUP BY snam;
+
+/*группировка по нескольким столбцам */
+SELECT 
+    snam, sdate, max(amount)
+FROM
+    sales
+GROUP BY snam, sdate;
+
+/*отбор значений после группировки
+ осуществляется с помощью специального выражения having
+ where отбирает строки до группировки и не может сордержать агрегатные функции*/
+SELECT 
+    snam, sdate, MAX(amount)
+FROM
+    sales
+GROUP BY snam , sdate
+HAVING MAX(amount) > 500;
+
+/*having не работает до группировки*/
+
+/*Если в группировке не используеьтся поле, по которому нужно сделать отбор, 
+то используется только выражение where*/
+SELECT 
+    snam, sdate, MAX(amount)
+FROM
+    sales
+where 
+	sdate> '2024-09-15'
+	GROUP BY snam , sdate;
+
+	/*использование in с having*/
+
+SELECT 
+    snam, sdate, MAX(amount)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-15'
+GROUP BY snam
+HAVING snam IN (101 , 106);
